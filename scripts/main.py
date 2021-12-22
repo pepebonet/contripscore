827c2badded2486138f172f62f5b68e4911b6600
josebonet <jose.bonet@irbbarcelona.org>
2021-12-22 14:28:47 +0100

    development of contrip score

---
+++ scripts/main.py
@@ -0,0 +1,50 @@
+import os
+import click
+
+
+def initial_idea(x, y, w1, w2):
+    return max(1, min(5, x + (y - 0.5) * w1) - (1 - y) / w2 * x)
+
+
+def compute_scotrip(x, y, w1, w2):
+    return min(5, x + (y - 0.5) * w1) - (1 - y) / w2 * x
+
+
+def compute_scotrip_now2(x, y, w1):
+    return min(5, x + (y - 0.5) * w1) - (1 - y) / (6 - x)
+
+
+
+# ------------------------------------------------------------------------------
+# CLICK
+# ------------------------------------------------------------------------------
+
+@click.command(short_help='script to compute the score of a tripadvisor rating')
+@click.option(
+    '-tr', '--tripadvisor_rating', default=5, 
+    help='rating of tripadvisor hotel'
+)
+@click.option(
+    '-cv', '--consensus_value', default=1.0, 
+    help='value of consensus analysis through NLP'
+)
+@click.option(
+    '-w1', '--weight_1', default=1.5, 
+    help='weight for the importance of the consensus value'
+)
+@click.option(
+    '-w2', '--weight_2', default=10.0, 
+    help='weight for the importance of the consensus value 2'
+)
+@click.option(
+    '-o', '--output', default='', help='output folder'
+)
+def main(tripadvisor_rating, consensus_value, weight_1, weight_2, output):
+    
+    score = compute_scotrip(tripadvisor_rating, consensus_value, weight_1, weight_2)
+    print(score)
+
+
+
+if __name__ == '__main__':
+    main()
